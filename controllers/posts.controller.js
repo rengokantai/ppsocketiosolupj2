c602a93e80858a0c04d305cb5b140616adf2b387
Hernan Y.Ke <yuriqiao@gmail.com>
2016-09-12 00:11:31 -0400

    setInterval

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -4,10 +4,23 @@ var posts=[{
 }]
 
 module.exports = (socket)=>{
-	for(var i=0;i<posts.length;i++){
-		socket.emit('post.add',posts[i]);
-		socket.emit('post.count',{
-			count:i+1
-		})
-	}
+
+	// for(var i=0;i<posts.length;i++){
+	// 	socket.emit('post.add',posts[i]);
+	// 	socket.emit('post.count',{
+	// 		count:i+1
+	// 	})
+	// }
+	var i=0;
+	var addingPosts = setInterval(()=>{
+		if(posts[i]){
+		 	socket.emit('post.add',posts[i]);
+			socket.emit('post.count',{
+ 			count:i+1
+		});
+		i++;
+	}else{
+		clearInterval(addingPosts);
+		}
+	},1000)
 }
